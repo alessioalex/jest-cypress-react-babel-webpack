92b3f3fa868acd59f51affa1ee8062990fe357d2
Alexandru Vladutu <github@social.alessioalex.com>
2020-04-20 16:26:12 +0300

    feat: support custom module resolution

---
+++ src/__tests__/calculator.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import Calculator from '../calculator'
+
+test('renders', () => {
+  render(<Calculator />)
+  // const {debug} = render(<Calculator />)
+
+  // debug()
+})
