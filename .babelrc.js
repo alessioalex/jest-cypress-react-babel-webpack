ade8a5d8b1401091b6b5a910d65f083fd78ad178
Alexandru Vladutu <github@social.alessioalex.com>
2020-04-20 15:55:48 +0300

    feat: import css styles with jest

--- .babelrc.js
+++ .babelrc.js
@@ -3,6 +3,8 @@ const isTest = String(process.env.NODE_ENV) === 'test'
 
 module.exports = {
   presets: [
+    // for production let webpack compile esm
+    // for testing env let babel compile esm (for jest)
     ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}],
     '@babel/preset-react',
     [
