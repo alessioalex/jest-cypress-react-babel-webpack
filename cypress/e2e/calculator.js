fb85dc06740e377e15a4b8b16c59280a6165b37a
Alexandru Vladutu <github@social.alessioalex.com>
2020-05-06 00:31:25 +0300

    initial setup: install cypress, configure it, add extra testing-library/cypress goodies, write some basic tests

---
+++ cypress/e2e/calculator.js
@@ -0,0 +1,10 @@
+describe('anonymous calculator', () => {
+  it('can make calculations', () => {
+    cy.visit('/')
+    cy.findByText(/^1$/).click()
+    cy.findByText(/^\+$/).click()
+    cy.findByText(/^2$/).click()
+    cy.findByText(/^=$/).click()
+    cy.findByTestId('total').should('have.text', '3')
+  })
+})
