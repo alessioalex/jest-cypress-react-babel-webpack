4dab02ab2c15e6692572121afd12784ebcc2ba8e
Alexandru Vladutu <alexandru.vladutu@gmail.com>
2020-05-06 17:53:59 +0300

    debug a test with cypress

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -4,6 +4,13 @@ describe('anonymous calculator', () => {
     cy.findByText(/^1$/).click()
     cy.findByText(/^\+$/).click()
     cy.findByText(/^2$/).click()
+    // .then(subject => {
+    //   debugger
+    //   return subject
+    // })
+    // or debug() / pause()
+    // or in your appcode if (window.Cypress) {...}
+    // .click()
     cy.findByText(/^=$/).click()
     cy.findByTestId('total').should('have.text', '3')
   })
