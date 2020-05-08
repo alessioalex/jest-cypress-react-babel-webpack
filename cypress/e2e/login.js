7efeda1082fd4c3da29b7c9bc48865cc4f320320
Alexandru Vladutu <alexandru.vladutu@gmail.com>
2020-05-08 12:54:37 +0300

    keep tests isolated and focused with custom cypress commands

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -1,27 +1,19 @@
-import {buildUser} from '../support/generate'
-
 describe('registration', () => {
   it('should login an existing user', () => {
-    const user = buildUser()
+    cy.createUser().then(user => {
+      // now our test can start
+      cy.visit('/')
+      cy.findByText(/login/i).click()
+      cy.findByLabelText(/username/i).type(user.username)
+      cy.findByLabelText(/password/i).type(user.password)
+      cy.findByText(/submit/i).click()
 
-    cy.request({
-      url: 'http://localhost:3000/register',
-      method: 'POST',
-      body: user,
+      // now let's verify things are set after login
+      cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+      cy.window()
+        .its('localStorage.token')
+        .should('be.a', 'string')
+      cy.findByTestId('username-display').should('have.text', user.username)
     })
-
-    // now our test can start
-    cy.visit('/')
-    cy.findByText(/login/i).click()
-    cy.findByLabelText(/username/i).type(user.username)
-    cy.findByLabelText(/password/i).type(user.password)
-    cy.findByText(/submit/i).click()
-
-    // now let's verify things are set after login
-    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
-    cy.window()
-      .its('localStorage.token')
-      .should('be.a', 'string')
-    cy.findByTestId('username-display').should('have.text', user.username)
   })
 })
